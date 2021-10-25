0d5d2a0fe3bc93a28a94d52280c81601d9ab48c9
rodriguesmarcus <marcusrodrigues2@gmail.com>
2021-10-25 19:26:47 -0300

    adição de pastas para as disciplinas front end e programação imperativa

---
+++ program-imp/ex.js
@@ -0,0 +1,11 @@
+// ex 1
+console.log('ola mundo!');
+
+// ex2
+let nome = 'marcus';
+let idade = 29;
+let usuarioAtivo = true;
+
+console.log('meu nome é', nome)
+console.log('tenho ', idade, ' anos')
+console.log('e estou ONLINE? ', usuarioAtivo)
