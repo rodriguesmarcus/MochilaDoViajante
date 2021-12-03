25e662a4d5e3b76c28c10a2e1a8b833549a65809
rodriguesmarcus <marcusrodrigues2@gmail.com>
2021-12-03 16:13:19 -0300

    atividade da mesa 17

--- program-imp/aula_17/mesa.js
+++ program-imp/aula_17/mesa.js
@@ -1,68 +1,104 @@
 // // exercicio.docx
 
-// let pares = [2, 4, 6, 8, 10, 12];
+let pares = [2, 4, 6, 8, 10, 12];
 
-// let impares = pares.map((num) => {
-//     return num+1;
-// });
+let impares = pares.map((num) => {
+    return num+1;
+});
 
 // console.log(pares);
 // console.log(impares);
 
-// let nomes = ['maria', 'maria', 'joao', 'marcus', 'rodrigo'];
+let nomes = ['maria', 'maria', 'joao', 'marcus', 'rodrigo'];
 
-// let filtraMaria = nomes.filter((nome) => {
-//     return nome ==='maria';
-// })
+let filtraMaria = nomes.filter((nome) => {
+    return nome ==='maria';
+})
 
 // console.log(nomes)
 // console.log(filtraMaria)
 
-// let numeros = [1, 23, 4, 5, 21, 14, 9];
+let numeros = [1, 23, 4, 5, 21, 14, 9];
 
-// let numeroFormatado = numeros.reduce((acc, num) => {
-//     return acc + ' – ' + num;
-// })
+let numeroFormatado = numeros.reduce((acc, num) => {
+    return acc + ' – ' + num;
+})
 
 // console.log(numeroFormatado)
 
-// let animais = ['vaca', 'galinha', 'pato', 'gato', 'cachorro'];
+let animais = ['vaca', 'galinha', 'pato', 'gato', 'cachorro'];
 
-// let printAnimais = animais.forEach(
-//     (animal) => {
-//         console.log('o animal é: ', animal);
-//     }
-// )
+let printAnimais = animais.forEach(
+    (animal) => {
+        console.log('o animal é: ', animal);
+    }
+)
 
 // printAnimais;
 
 // mesa de trabalho
+//1
 
 let listaNumeros = [10, 20];
 
 let novaLista = listaNumeros.map(
     (numero) => {
         return numero / listaNumeros.reduce(
-            (acc, num) => { return acc + num } 
+            (acc, num) => {
+                return acc + num
+            }
         )
     }
 )
 
-console.log(novaLista);
+// console.log(novaLista);
 
+
+//2
 let listaMista = ['vaca', 'galinha', 'pato', 'gato', 'cachorro', 1];
 
 let novaLista2 = listaMista.filter(
     (item) => {
-        return item.length > 0;      
+        return item.length > 0;
     }
-) 
-
-console.log(novaLista2)
-
-let listaLiterais = [{nome:'marcus',nota:10}, {nome:'jesus',nota:6}];
-
-let listaCresc = listaLiterais.sort(
-    (a,b) => {return a - b}
 )
-console.log
+
+// console.log(novaLista2)
+
+// 3
+let listaLiterais = [{
+    nome: 'marcus',
+    nota: 10
+}, {
+    nome: 'jesus',
+    nota: 6
+}, {
+    nome: 'ana',
+    nota: 8
+}];
+
+let listaCrescNome = listaLiterais.sort(
+    (a, b) => {
+        return a.nome.localeCompare(b.nome)
+    }
+)
+
+// console.log(listaCrescNome)  
+
+let listaCrescNota = listaLiterais.sort(
+    (a, b) => {
+        return b.nota - a.nota
+    }
+)
+
+// console.log(listaCrescNota)
+
+// Exercício integrador
+
+// function VendasAgro(nomeProduto, precoUnitario, qtdVendida) {
+//     this.nomeProduto = nomeProduto;
+//     this.precoUnitario = precoUnitario;
+//     this.qtdVendida = qtdVendida;
+// }
+
+// let 
