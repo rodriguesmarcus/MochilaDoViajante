797f4a19d9bc1ee00498d8aef5309d3161fd5bf0
rodriguesmarcus <marcusrodrigues2@gmail.com>
2021-12-08 20:37:24 -0300

    edição lista alunos

--- program-imp/checkpoint_ii/listaEstudantes.js
+++ program-imp/checkpoint_ii/listaEstudantes.js
@@ -1,10 +1,10 @@
 listaEstudantes = [
-    { nome: "rodrigo rodrigues", qtdFaltas: 1, notas: [10, 5, 10] },
-    { nome: "marcus rodrigues", qtdFaltas: 1, notas: [10, 8, 9] },
+    { nome: "rodrigo rodrigues", qtdFaltas: 4, notas: [10, 5, 10] },
+    { nome: "marcus rodrigues", qtdFaltas: 3, notas: [10, 8, 9] },
     { nome: "bruno neves", qtdFaltas: 2, notas: [8.7, 8.8, 8.8] },
-    { nome: "mateus praxedes", qtdFaltas: 2, notas: [8.7, 8.8, 8.8] },
-    { nome: "moises bollela", qtdFaltas: 2, notas: [8.7, 8.8, 8.8] },
-    { nome: "wellington siqueira", qtdFaltas: 2, notas: [8.7, 8.8, 8.8] }
+    { nome: "mateus praxedes", qtdFaltas: 3, notas: [8.7, 8.8, 8.8] },
+    { nome: "moises bollela", qtdFaltas: 2, notas: [7, 8.8,10] },
+    { nome: "wellington siqueira", qtdFaltas: 4, notas: [8.7, 8.8, 8.8] }
 ];
 
 
